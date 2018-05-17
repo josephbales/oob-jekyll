d1ca61ab40bf46792445c3c225a93ac479323eba
Joseph Bales <joseph.bales@gmail.com>
2018-05-16 19:51:44 -0500

    more theme changes

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,16 @@
 $(function() {
-    console.log( "ready!" );
+    if (isBlogPath())
+    {
+        $('#nav-blog').addClass('active');
+    }
+
+    function isBlogPath() {
+        var pathname = window.location.pathname;
+        var regex = /^\/blog.*/gi;
+        if (regex.test(pathname)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 });
