712ea8b425e6442445d470c478bf9a639e6fc46b
Joseph Bales <joseph.bales@gmail.com>
2018-05-20 21:30:43 -0500

    random post

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,7 +2,7 @@ $(function() {
     if (isBlogPath())
     {
         $('#nav-blog').addClass('active');
-    };
+    }
 
     function isBlogPath() {
         var pathname = window.location.pathname;
@@ -12,9 +12,13 @@ $(function() {
         } else {
             return false;
         }
-    };
+    }
 
     $('.random-post').ready(function() {
+        getRandomPost();
+    });
+
+    $('#showAnotherRandomPost').on("click", function() {
         getRandomPost();
     });
 
@@ -24,30 +28,60 @@ $(function() {
             url: jsonUrl,
             success: _onBlogJsonSuccess
         });
-    };
+    }
 
     function _onBlogJsonSuccess(data) {
+        $('#carouselInnerRandomPostTitles').empty();
+
         var randInt = _getRandomInt(0, data.length - 1);
-        var post = data[randInt];
-        $('#random-post-title').html(post.title);
-        $('#random-post-date').html(post.date);
-        _getBlogContent(post.url);
 
-        // maybe spin through post titles before proceeding
+        var firstDataChunk = data.slice(randInt);
+        var lastDataChunk = data.slice(0, randInt);
 
-        // also maybe a button to kick it off manually
-    };
+        var reorderedData = firstDataChunk.concat(lastDataChunk);
 
-    function _getBlogContent(url) {
-        $.get(url, function(data) {
+        _blogTitleSpinner(reorderedData, randInt);
+
+        var lastPost = reorderedData.pop();
+
+        $('.carousel').carousel({
+            interval: 10,
+            pause: false,
+            wrap: false
+        })
+
+        setTimeout(function(){ _getBlogContent(lastPost); }, 35 * data.length);
+    }
+
+    function _getTitle(item, index) {
+        return item.title;
+    }
+
+    function _blogTitleSpinner(data, postNum) {
+        var dataLen = data.length - 1;
+        var nextPostNum = postNum + 1;
+
+        var titles = data.map(_getTitle);
+
+        $.each(titles, function(index, value) {
+            $('#carouselInnerRandomPostTitles').append('<div class="carousel-item custom-carousel-item"><h2 class="random-post-title">' + value + '</h2></div>');
+            if (index === 0) {
+                $('.carousel-item').addClass('active');
+            }
+        });
+    }
+
+    function _getBlogContent(post) {
+        $.get(post.url, function(data) {
             var _html = $(data);
-            $('#random-post-content').html(_html.find('.post-content').html());
+            $('.random-post-date').text('Post Date: ' + post.date);
+            $('.random-post-content').html(_html.find('.post-content').html());
         });
     }
 
     function _getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
+    }
 
     function _getHost() {
         return window.location.protocol + '//' + window.location.host;
